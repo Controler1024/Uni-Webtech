6ca5ef5b65845df317b42bc1ca550d3aa9953580
Controler1024 <57942068+Controler1024@users.noreply.github.com>
2019-11-19 15:11:25 +0100

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,7 +1,7 @@
 document.getElementById("app").innerHTML = `
 <h1>Hallo Marcus!</h1>
 <div>
-  Wie geht es dir?
+  Wie geht es dir?<br>mir egal
   <br><br>We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
