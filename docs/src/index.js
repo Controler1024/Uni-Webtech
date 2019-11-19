d1d4062d6a9fbed471598a7b94bdba6bc6b57e23
Controler1024 <57942068+Controler1024@users.noreply.github.com>
2019-11-19 15:48:51 +0100

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -3,5 +3,6 @@ document.getElementById("app").innerHTML = `
 <div>
   We used to be humans, but since we 'work' for Deutsche Telekom, we have died inside.
   <br>You can find more about our diseases <a href="https://www.amazon.de/gp/product/383481427X/ref=dbs_a_def_rwt_hsch_vapi_taft_p1_i2" target="_blank" rel="noopener noreferrer">here</a>.
+<br>
 </div>
 `;
