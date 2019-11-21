1f0bb5aad3d09b951005b90b71e6654b62056193
Controler1024 <57942068+Controler1024@users.noreply.github.com>
2019-11-21 14:35:08 +0100

    viral changes
    everything

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,8 +1,11 @@
 document.getElementById("app").innerHTML = `
-<h1>Hallo Mankind!</h1>
+<h1>Hello Mankind!</h1>
 <div>
   We used to be humans, but since we 'work' for Deutsche Telekom, we have died inside.
-  <br>You can find more about our diseases <a href="https://www.amazon.de/gp/product/383481427X/ref=dbs_a_def_rwt_hsch_vapi_taft_p1_i2" target="_blank" rel="noopener noreferrer">here</a>.
-<br>
+  <br>You can find more about our diseases 
+    <a href="https://www.amazon.de/gp/product/383481427X/ref=dbs_a_def_rwt_hsch_vapi_taft_p1_i2" target="_blank" rel="noopener noreferrer">
+      here</a>
+    or
+    <a href="https://epdf.pub/download/bilddatenkompression-grundlagen-codierung-wavelets-jpeg-mpeg-h264.html">here</a>.    <br>
 </div>
 `;
