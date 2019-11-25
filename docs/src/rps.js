4cd7130d8a40f8b45fc67fd9bbd89360b855c715
Griese009 <32206327+Griese009@users.noreply.github.com>
2019-11-25 21:53:54 +0000

    Update rps.js

--- docs/src/rps.js
+++ docs/src/rps.js
@@ -1,7 +1,7 @@
 document.getElementById("rps");
 
 function getanswer(message) {
-  var number = Math.round(Math.random()*3).toString();
+  var number = Math.round(Math.random()*2).toString(); // generates a random number between 0 and 2 (for 3 cases: rock, paper, scissor)
   var penis = message + " " + number;
   alert(penis);
 }
