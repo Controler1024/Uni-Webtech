09833b99cd8167cdab4650a69d3c05aa951b42c4
Controler1024 <57942068+Controler1024@users.noreply.github.com>
2019-11-25 12:50:51 +0100

    new site, minispiele
    versuch von inner html und javaskript

---
+++ docs/src/rps.js
@@ -0,0 +1,50 @@
+document.getElementsByName("rps").innerHTML = `
+<table border="1" cellpadding="5">
+      <th>
+        Spieler 1
+      </th>
+      <th>
+        Spieler 2
+      </th>
+
+      <tr>
+        <td>
+          <button type="button" class="rps">
+            Schere
+          </button>
+        </td>
+        <td>
+          <button type="button">
+            Schere
+          </button>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <button type="button">
+            Stein
+          </button>
+        </td>
+        <td>
+          <button type="button">
+            Stein
+          </button>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <button type="button">
+            Papier
+          </button>
+        </td>
+        <td>
+          <button type="button">
+            Papier
+          </button>
+        </td>
+      </tr>
+      
+    </table>
+    `;
