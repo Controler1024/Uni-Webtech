f17fbe94018c1a7c64fea38facf80378c9a89211
Controler1024 <57942068+Controler1024@users.noreply.github.com>
2019-11-25 15:46:06 +0100

    testing button actions

--- docs/src/rps.js
+++ docs/src/rps.js
@@ -1,50 +1,6 @@
-document.getElementById("rps").innerHTML = `
-<table border="1" cellpadding="5">
-      <th>
-        Spieler 1
-      </th>
-      <th>
-        Spieler 2
-      </th>
+document.getElementById("rps");
 
-      <tr>
-        <td>
-          <button type="button">
-            Schere
-          </button>
-        </td>
-        <td>
-          <button type="button">
-            Schere
-          </button>
-        </td>
-      </tr>
-
-      <tr>
-        <td>
-          <button type="button">
-            Stein
-          </button>
-        </td>
-        <td>
-          <button type="button">
-            Stein
-          </button>
-        </td>
-      </tr>
-
-      <tr>
-        <td>
-          <button type="button">
-            Papier
-          </button>
-        </td>
-        <td>
-          <button type="button">
-            Papier
-          </button>
-        </td>
-      </tr>
-      
-    </table>
-    `;
+function getanswer(message) {
+  var penis = message;
+  alert(penis);
+}
---
+++ docs/src/test.js
@@ -0,0 +1,1 @@
+alert("do not anger me!");
