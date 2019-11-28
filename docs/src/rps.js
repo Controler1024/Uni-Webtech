5c16c1e0be7d6ca7852dddd20d4012f6604d082d
griese008 <57891476+griese008@users.noreply.github.com>
2019-11-28 08:56:25 +0100

    Update rps.js

--- docs/src/rps.js
+++ docs/src/rps.js
@@ -1,7 +1,30 @@
 document.getElementById("rps");
 
-function getanswer(message) {
+function getanswer(decision) {
+  
   var number = Math.round(Math.random()*2).toString(); // generates a random number between 0 and 2 (for 3 cases: rock, paper, scissor)
-  var penis = message + " " + number;
-  alert(penis);
+  
+  if(decision = number)
+  {
+    alert("draw");
+  }
+  else if (decision != number)
+  {
+    /*
+    1 Scissor
+    2 Stone
+    3 Paper
+    */
+    if(decision == 3)
+    {
+      if(number == 1)
+      {
+        alert("You lost!");
+      }
+      else
+      {
+        alert("You won!");
+      }
+    }
+  }
 }
